3e6e987851a29f5defd89f3c39617370b83401d1
De Sean Ward <dward1711@gmail.com>
2023-07-11 02:36:08 -0500

    Wild Wild Life Answered

--- lab-answers.js
+++ lab-answers.js
@@ -46,4 +46,49 @@ console.log(' ');
  
  console.log(str3);
  console.log(' ');
+
+
+ /**
+ * ****************************************************** WILD WILD LIFE *********************************************
+ */
+//TODO: ***** Should I convert to objects? *****
+console.log('********** WILD WILD LIFE **********');
+const wolfy = ['Wolfy', 'wolf', 16, 'Yukon Territory'];
+const sharky = ['Sharky', 'shark', 20, 'Left Coast'];
+const plantee = ['Plantee', 'plant', 5000, 'Mordor'];
+const porgee = ['Porgee', 'Porg', 186, 'Ahch-To'];
+const dart = ["D'Art", 'Demogorgan Dog', 2, 'Upside Down'];
+
+//1. Update Plantee's birthday
+console.log("1. Update Plantee's Birthday One Year");
+console.log(plantee);
+
+plantee[2] += 1;
+console.log(plantee);
+console.log(' ');
+
+//2. Change Wolfy's hometown
+console.log("2. Change Wolfy's Hometown From 'Yukon Territory'");
+console.log(wolfy);
+
+wolfy[3] = 'Gotham City';
+console.log(wolfy);
+console.log(' ');
+
+//3. D'Art's second hometown
+console.log("3. Add D'Art's Second Hometown of 'Hawkins'");
+console.log(dart);
+
+dart[3] += ' & Hawkins';
+console.log(dart);
+console.log(' ');
+
+//4. Remove and replace 'Wolfy'
+console.log("4. Remove And Replace 'Wolfy' With 'Gameboy'");
+console.log(wolfy);
+
+wolfy.splice(0, 1, 'Gameboy');
+console.log(wolfy);
+console.log(' ');
+
  
