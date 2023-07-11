673d5b5d585c5149bda8c5e279ef0638497f886d
De Sean Ward <dward1711@gmail.com>
2023-07-11 02:22:52 -0500

    Easy Going Answered

---
+++ lab-answers.js
@@ -0,0 +1,13 @@
+console.log('running...');
+/**
+ * ***************************************************** EASY GOING ******************************************
+ */
+ console.log('********** EASY GOING **********');
+ let str = '';
+ 
+ for (let i = 1; i <= 20; i++) {
+     str += i + ' ';
+ }
+ 
+ console.log(str);
+ console.log(' ');
