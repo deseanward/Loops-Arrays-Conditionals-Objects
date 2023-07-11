6d3d8487a54825fa8995d3c6711745f946e1b660
De Sean Ward <dward1711@gmail.com>
2023-07-11 02:43:15 -0500

    Methods Revisited Answered

--- lab-answers.js
+++ lab-answers.js
@@ -1,17 +1,16 @@
 /**
  * ***************************************************** EASY GOING ******************************************
  */
- console.log('********** EASY GOING **********');
- let str = '';
- 
- for (let i = 1; i <= 20; i++) {
-     str += i + ' ';
- }
- 
- console.log(str);
- console.log(' ');
+console.log('********** EASY GOING **********');
+let str = '';
 
- 
+for (let i = 1; i <= 20; i++) {
+	str += i + ' ';
+}
+
+console.log(str);
+console.log(' ');
+
 /**
  * ******************************************************* GET EVEN *********************************************
  */
@@ -27,28 +26,26 @@ for (let i = 0; i <= 200; i++) {
 console.log(str2);
 console.log(' ');
 
-
 /**
  * ****************************************************** FIZZ BUZZ *********************************************
  */
- console.log('********** FIZZ BUZZ **********');
- let str3 = '';
- 
- for (let i = 1; i <= 100; i++) {
-     i % 3 === 0 && i % 5 === 0
-         ? (str3 += 'FizzBuzz, ')
-         : i % 5 === 0
-         ? (str3 += 'Buzz, ')
-         : i % 3 === 0
-         ? (str3 += 'Fizz, ')
-         : (str3 += i + ', ');
- }
- 
- console.log(str3);
- console.log(' ');
+console.log('********** FIZZ BUZZ **********');
+let str3 = '';
 
+for (let i = 1; i <= 100; i++) {
+	i % 3 === 0 && i % 5 === 0
+		? (str3 += 'FizzBuzz, ')
+		: i % 5 === 0
+		? (str3 += 'Buzz, ')
+		: i % 3 === 0
+		? (str3 += 'Fizz, ')
+		: (str3 += i + ', ');
+}
 
- /**
+console.log(str3);
+console.log(' ');
+
+/**
  * ****************************************************** WILD WILD LIFE *********************************************
  */
 //TODO: ***** Should I convert to objects? *****
@@ -91,17 +88,94 @@ wolfy.splice(0, 1, 'Gameboy');
 console.log(wolfy);
 console.log(' ');
 
-
 /**
  * ****************************************************** YELL AT THE NINJA TURTLES *********************************************
  */
- console.log('********** YELL AT THE NINJA TURTLES **********');
- const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
- 
- for (let name of turtles) {
-     console.log(name.toUpperCase() + '!!!');
- }
- 
- console.log(' ');
- 
- 
+console.log('********** YELL AT THE NINJA TURTLES **********');
+const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+
+for (let name of turtles) {
+	console.log(name.toUpperCase() + '!!!');
+}
+
+console.log(' ');
+
+/**
+ * ****************************************************** METHODS, REVISITED
+ **********************************************
+ */
+console.log('********** METHODS REVISITED ********');
+const favMovies = [
+	'Jaws',
+	'The Fellowship of the Ring',
+	"Howl's Moving Castle",
+	'Django Unchained',
+	'Cloud Atlas',
+	'The Usual Suspects',
+	'Toy Story',
+	'Conan the Barbarian',
+	'Titanic',
+	'Harry Potter',
+	'Fried Green Tomatoes',
+	'Volver',
+	'Oculus',
+	'Seven',
+	'Black Panther',
+	'Harry Potter',
+	'Imitation of Life',
+	'Snatch',
+	'Fast and Furious',
+];
+
+console.log('Original Array:');
+console.log(
+	"const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl's Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];"
+);
+console.log(' ');
+
+// The index of Titanic
+let idx = favMovies.indexOf('Titanic');
+console.log(`The index of Titanic is ${idx}`);
+console.log(' ');
+
+// Sort the array
+favMovies.sort();
+
+// Pop the array
+favMovies.pop();
+
+// Push to the array
+favMovies.push('Guardians of the Galaxy');
+
+//Reverse the array
+favMovies.reverse();
+
+// Shift the array
+favMovies.shift();
+
+// Un-shift the array
+let c = favMovies.unshift('Test Movie Inserted');
+console.log('After unshift, it returns the length. In this case: ', c); // Returns the length of the new array
+console.log(' ');
+
+// Splice the array
+let idx2 = favMovies.indexOf('Django Unchained');
+favMovies.splice(idx2, 0, 'Advatar');
+
+let len = favMovies.length;
+let midpoint = len / 2;
+
+let movieSliced = favMovies.slice(midpoint, favMovies.length);
+
+console.log('New sliced array:');
+console.log(movieSliced);
+console.log(' ');
+
+console.log(
+	'The index of "Fast and Furious" is',
+	favMovies.indexOf('Fast and Furious')
+);
+console.log(
+	'It was never removed due to the "sort()" method. "Volver" was "popped" from the end.'
+);
+console.log(' ');
